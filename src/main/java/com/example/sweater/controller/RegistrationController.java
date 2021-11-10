3da5da1be9e2271d2e4277b378c8bf0161def0d3
skolesnikov <chimbosan@yandex.ru>
2021-11-10 23:05:32 +0300

    Spring Boot: reCaptcha, rest client, rememberMe и сохранение сессий в БД

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -1,23 +1,38 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.domain.User;
+import com.example.sweater.domain.dto.CaptchaResponseDto;
 import com.example.sweater.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class RegistrationController {
 
+  public static final String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
   private final UserService userService;
+
+  @Value("${recaptcha.secret}")
+  private String secret;
+
+  @Autowired
+  public RestTemplate restTemplate;
 
   @GetMapping("/registration")
   public String registration() {
@@ -25,12 +40,27 @@ public class RegistrationController {
   }
 
   @PostMapping("/registration")
-  public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-    if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+  public String addUser(
+          @RequestParam("password2") String passwordConfirmation,
+          @RequestParam("g-recaptcha-response") String captchaResponse,
+          @Valid User user,
+          BindingResult bindingResult,
+          Model model) {
+    String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+    CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+    if (!response.isSuccess()) {
+      model.addAttribute("captchaError", "Fill captcha");
+    }
+    boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirmation);
+    if (isConfirmEmpty) {
+      model.addAttribute("password2Error", "Password confirmation cannot be empty");
+    }
+    if (user.getPassword() != null && !user.getPassword().equals(passwordConfirmation)) {
       model.addAttribute("passwordError", "Passwords are different");
     }
 
-    if (bindingResult.hasErrors()) {
+    if (isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()) {
       Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
       model.mergeAttributes(errors);
       return "registration";
@@ -47,8 +77,10 @@ public class RegistrationController {
   public String activate(Model model, @PathVariable String code) {
     boolean isActivate = userService.activateUser(code);
     if (!isActivate) {
+      model.addAttribute("messageType", "success");
       model.addAttribute("message", "User success fully activate");
     } else {
+      model.addAttribute("messageType", "danger");
       model.addAttribute("message", "Activation code is not found!");
     }
     return "login";
--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -24,13 +24,10 @@ public class User implements UserDetails {
 
   @NotBlank(message = "Password cannot be empty")
   private String password;
-  @Transient
-  @NotBlank(message = "Password confirmation cannot be empty")
-  private String password2;
   private boolean active;
 
   @Email(message = "Email is not correct")
-  @NotBlank(message = "Email cannot be empty")
+//  @NotBlank(message = "Email cannot be empty")
   private String email;
   private String activationCode;
 
